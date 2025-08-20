1bc5da757ae9e9d19b39f07d79540ee940395fdb
RafaelaLeite02 <rafaelaleite2017@icloud.com>
2025-08-20 10:55:58 -0300

    Add files via upload

---
+++ Employee contract/Exercicio-20-08/bin/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_20_08.Entities
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2,
+    }
+}
