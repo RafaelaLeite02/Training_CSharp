a212d3009ff05a3d7b6b0800a32d725d0d87556a
RafaelaLeite02 <rafaelaleite2017@icloud.com>
2025-08-20 11:48:14 -0300

    Add files via upload

---
+++ PostSocial/Exercise_PostSocial/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Exercise_PostSocial.Entities
+{
+     class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
