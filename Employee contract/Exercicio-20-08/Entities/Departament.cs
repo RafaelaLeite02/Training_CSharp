d8b9cc00c625b782c3aeb5b9bfc66c53a555d8a5
RafaelaLeite02 <rafaelaleite2017@icloud.com>
2025-08-20 10:59:12 -0300

    Add files via upload

---
+++ Employee contract/Exercicio-20-08/Entities/Departament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_20_08.Entities.Enums
+{
+    internal class Departament
+    {
+        public string Name { get; set; }
+
+        public Departament()
+        {
+        }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
